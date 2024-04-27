3224b2a83bcf4c2aa0252d2eadfe1d3f2262f620
JINJU <devpearlkim@gmail.com>
2024-04-27 17:04:21 +0900

    feat: add persist to authStore, add OG, fix title in index.html

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,10 +1,13 @@
 import { create } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
 
-const useAuthStore = create((set) => ({
+const authStore = create((set) => ({
   user: null,
   login: (user) => set({ user }),
   logout: () => set({ user: null }),
   setUser: (user) => set({ user }),
 }));
 
-export default useAuthStore;
+export const useAuthStore = create(
+  persist(devtools(authStore), { name: 'authStore' })
+);
