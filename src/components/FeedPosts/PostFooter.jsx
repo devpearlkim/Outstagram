b85250423cff45ed7f89bc94f1dab5b904cc9103
JINJU <devpearlkim@gmail.com>
2024-04-30 16:43:52 +0900

    feat: profile page posts, post css

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,9 +1,9 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Input, InputGroup, Text } from '@chakra-ui/react';
 import { useState } from 'react';
 import { FaRegComment } from 'react-icons/fa';
 import { IoMdHeart, IoMdHeartEmpty } from 'react-icons/io';
 
-export default function PostFooter({ username }) {
+export default function PostFooter({ username, isProfilePage }) {
   const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(100);
   const handleLike = () => {
@@ -18,30 +18,57 @@ export default function PostFooter({ username }) {
 
   return (
     <>
-      <Flex alignItems={'center'} gap={4} w={'full'} pt={0} my={2}>
-        <Box onClick={handleLike} cursor={'pointer'}>
-          {liked ? (
-            <IoMdHeartEmpty size={25} />
-          ) : (
-            <IoMdHeart size={25} color='red' />
-          )}
-        </Box>
-        <Box cursor={'pointer'}>
-          <FaRegComment size={20} />
-        </Box>
-      </Flex>
-      <Text fontSize={'sm'} fontWeight={600}>
-        {likes} likes
-      </Text>
-      <Text fontSize={'sm'} fontWeight={700}>
-        {username}{' '}
-        <Text as={'span'} fontWeight={400}>
-          날씨 좋다
+      <Box mb={10} marginTop={'auto'}>
+        <Flex alignItems={'center'} gap={4} w={'full'} pt={0} my={2}>
+          <Box onClick={handleLike} cursor={'pointer'}>
+            {liked ? (
+              <IoMdHeartEmpty size={25} />
+            ) : (
+              <IoMdHeart size={25} color='red' />
+            )}
+          </Box>
+          <Box cursor={'pointer'}>
+            <FaRegComment size={20} />
+          </Box>
+        </Flex>
+        <Text fontSize={'sm'} fontWeight={600}>
+          {likes} likes
         </Text>
-      </Text>
-      <Text fontSize={'sm'} color={'gray.700'}>
-        View all comments
-      </Text>
+        {!isProfilePage && (
+          <>
+            {' '}
+            <Text fontSize={'sm'} fontWeight={700}>
+              {username}{' '}
+              <Text as={'span'} fontWeight={400}>
+                날씨 좋다
+              </Text>
+            </Text>
+            <Text fontSize={'sm'} color={'gray.700'}>
+              View all comments
+            </Text>
+          </>
+        )}
+        <Flex
+          alignItems={'center'}
+          gap={2}
+          justifyContent={'space-between'}
+          w={'full'}
+        >
+          <InputGroup>
+            <Input placeholder={'Add a comment...'} fontSize={14} />
+            <Button
+              fontSize={14}
+              color={'blue.500'}
+              fontWeight={600}
+              cursor={'pointer'}
+              _hover={{ color: 'white' }}
+              bg={'transparent'}
+            >
+              Post
+            </Button>
+          </InputGroup>
+        </Flex>
+      </Box>
     </>
   );
 }
